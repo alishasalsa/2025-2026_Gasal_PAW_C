6ef8d02381585f91305edae0aab762e723f8408e
alishasalsa <153835742+alishasalsa@users.noreply.github.com>
2025-10-14 23:09:27 +0600

    Add files via upload

---
+++ Modul3/23-118_Alisha_salsabila_achmad/3.3.3.BuatArrayBaruDenganNama.php
@@ -0,0 +1,11 @@
+<?php
+$weight = array(
+    "Andy" => 65,
+    "Bany" => 60,
+    "Charlie" => 63
+);
+
+$values = array_values($weight);
+
+echo "Data ke-2 dari array \$weight adalah: " . $values[1] . " kg";
+?>
